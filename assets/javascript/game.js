788ac173f0cd4cf9f01cb4d91d65f4c04d330f2b
ianschu <ianschu7@gmail.com>
2018-11-12 20:42:10 -0500

    basic css styling

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+// remember to console.log!
+
+// Wins counter
+var winsNumber ;
+var youWin = true;
+
+if (youWin) {
+    winsNumber = winsNumber++;
+    youWin = false;
+    console.log(winsNumber);
+}
+else {
+    console.log("you lose")
+}
+
+var animalArray = ["elephant", "ostrich", "vulture", "squirrel", "leopard", "termite", "orangutang", "crocodile", "aligator"];
+var wordToGuess ;
+var wordLength ; 
