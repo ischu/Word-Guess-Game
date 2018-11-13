c2a3e870d2a2834c62bae904dd95164733aa997e
ianschu <ianschu7@gmail.com>
2018-11-12 23:29:53 -0500

    basic JS for counters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,30 @@
 // remember to console.log!
 
 // Wins counter
-var winsNumber ;
+var winCount = 0;
 var youWin = true;
 
-if (youWin) {
-    winsNumber = winsNumber++;
-    youWin = false;
-    console.log(winsNumber);
-}
-else {
-    console.log("you lose")
+function winCounter() {
+    if (youWin) {
+        winCount++;
+        console.log(winCount);
+        youWin=false;
+        document.getElementById("winNum").innerHTML=winCount;
+    }
+    else {
+        console.log("you lose");
+    }
+};
+// Guess counter
+var guessCount = 15;
+function guessCounter(){
+    guessCount--;
+    console.log(guessCount);
+    document.getElementById("guessNum").innerHTML=guessCount;
 }
 
-var animalArray = ["elephant", "ostrich", "vulture", "squirrel", "leopard", "termite", "orangutang", "crocodile", "aligator"];
-var wordToGuess ;
-var wordLength ; 
+var animalArray=["elephant", "ostrich", "squirrel", "leopard", "termite", "orangutang", "crocodile", "aligator",
+    "zebra", "gorilla", "toucan", "dolphin", "peacock", "flamingo", "donkey"];
+var theWord;
+
+// winCounter()
