dd41ef8a7d32fc03e677f37cd004629eb380845f
ianschu <ianschu7@gmail.com>
2018-11-13 00:40:12 -0500

    added basic key function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,8 +8,8 @@ function winCounter() {
     if (youWin) {
         winCount++;
         console.log(winCount);
-        youWin=false;
-        document.getElementById("winNum").innerHTML=winCount;
+        youWin = false;
+        document.getElementById("winNum").innerHTML = winCount;
     }
     else {
         console.log("you lose");
@@ -17,13 +17,37 @@ function winCounter() {
 };
 // Guess counter
 var guessCount = 15;
-function guessCounter(){
+function guessCounter() {
     guessCount--;
     console.log(guessCount);
-    document.getElementById("guessNum").innerHTML=guessCount;
-}
+    document.getElementById("guessNum").innerHTML = guessCount;
+};
 
-var animalArray=["elephant", "ostrich", "squirrel", "leopard", "termite", "orangutang", "crocodile", "aligator",
+// key press stuff
+
+var letterArray = [];
+
+document.onkeyup = function (_event) {
+    // checks if key pressed is a letter
+    if (/[a-z]/.test(_event.key)) {
+        // sets theLetter to the pressed key
+        var theLetter = _event.key;
+        console.log("you pressed " + theLetter);
+        // reduces guess counter
+        guessCounter();
+        // add theLetter to guessed letters array
+        letterArray.push(theLetter);
+        // adds letter to 
+        document.getElementById("guessLett").innerHTML = letterArray;
+        return theLetter;
+    }
+    else {
+        alert("press a letter key!");
+    }
+};
+
+// Word Selection
+var animalArray = ["elephant", "ostrich", "squirrel", "leopard", "termite", "orangutang", "crocodile", "aligator",
     "zebra", "gorilla", "toucan", "dolphin", "peacock", "flamingo", "donkey"];
 var theWord;
 
